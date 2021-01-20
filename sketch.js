a148dc4e411d7a40ceb0c26bdae437b83565c6a7
y1430 <70380379+y1430@users.noreply.github.com>
2021-01-20 10:36:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var dog,happyDog,database,foodStock;
+var foodRemaining=0;
+
+function preload()
+{
+  dog=loadImage("images/dogImg.png");
+  happyDog=loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+  if (keyWentDown(UP_ARROW)){
+    writeStocks(foodS);
+    dog.addimge(happyDog);
+  }
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+}
+
+
+function draw() { 
+  background("green");
+  image(dog,300,250,150,150);
+
+  drawSprites();
+  
+  textSize(15);
+  fill("white");
+  text("Note: Press Up Arrow key to feed milk to Bruno",100,50);
+  text("Food Remaining: ",100,150);
+
+}
+
+function writeStocks(x) {
+  if(x<=0){
+    x=0;
+  }
+  else {
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
+
